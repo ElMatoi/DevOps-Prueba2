0577dd63afd498a8f89ad4f569247d4a7dffe98a
MatiasRh <matiasroahuenul@outlook.es>
2025-06-27 09:00:40 -0400

    feat (app): add initial text response endpoint

---
+++ src/app.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+   @Get()
+  getInicio(): string {
+    return 'Hola este es una cadena de texto, saludos al dazer :v';
+  }
+}
